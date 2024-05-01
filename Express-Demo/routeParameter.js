de59431b0247a7cfbba27083d1a022b853d42889
Vishal Tiwari <140997042+Vishal-Tiwari-11@users.noreply.github.com>
2024-05-01 15:10:02 +0530

    Update routeParameter.js

--- Express-Demo/routeParameter.js
+++ Express-Demo/routeParameter.js
@@ -1,17 +1,26 @@
 const express= require('express');
 const app=express();
 
+const courses=[
+    {id:1, name: 'course 1'},
+    {id:2, name: 'course 2'},
+    {id:3, name: 'course 3'},
+]
+
+
 app.get('/', (req,res)=>{
     res.send("Hello World!");
 });
 
 app.get('/api/courses', (req,res)=>{
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 // required: /api/courses/1
-app.get ('/api/courses/:year/:month', (req,res)=>{
-    res.send(req.params)
+app.get ('/api/courses/:id', (req,res)=>{
+    var course = courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course) res.status(404).send("The course with the given ID was not found.")
+    res.send(course)
 })
 
 
